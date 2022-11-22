353a9080fe4775359c8415f113bacdf6647079e6
MarioASG <mariosanhuezag@hotmail.com>
2022-11-22 20:33:05 -0300

    9 tragos

--- src/components/carta/listaDrinks.js
+++ src/components/carta/listaDrinks.js
@@ -8,21 +8,63 @@ const ListaDrinks = () => {
             nombre: 'HTML Sour',
             descripcion: 'Trago muy rico como tú',
             precio: '$3600',
-            imgDrink: 'assets/drinks/1.png'
+            imgDrink: 'assets/drinks/HTMLSour.png'
         },
         {
             id: 2,
             nombre: 'JavaSdrink',
             descripcion: 'Copete, mina, locura',
             precio: '$4500',
-            imgDrink: 'assets/drinks/2.png'
+            imgDrink: 'assets/drinks/javasDrink.png'
         },
         {
             id: 3,
-            nombre: 'Cí! Ese Ese!',
+            nombre: 'Cí! Ese Es!',
             descripcion: 'Amargór en tu paladar',
             precio: '$5100',
-            imgDrink: 'assets/drinks/3.png'
+            imgDrink: 'assets/drinks/cieses.png'
+        },
+        {
+            id: 4,
+            nombre: 'Node Nada',
+            descripcion: 'Trago muy rico como tú',
+            precio: '$3600',
+            imgDrink: 'assets/drinks/nodeNada.png'
+        },
+        {
+            id: 5,
+            nombre: 'Dom DeVivo',
+            descripcion: 'Copete, mina, locura',
+            precio: '$4500',
+            imgDrink: 'assets/drinks/domDevivo.png'
+        },
+        {
+            id: 6,
+            nombre: 'Prop Oxidado',
+            descripcion: 'Amargór en tu paladar',
+            precio: '$5100',
+            imgDrink: 'assets/drinks/propOxidado.png'
+        },
+        {
+            id: 7,
+            nombre: 'Reactivo Blue',
+            descripcion: 'Trago muy rico como tú',
+            precio: '$3600',
+            imgDrink: 'assets/drinks/reactivoBlue.png'
+        },
+        {
+            id: 8,
+            nombre: 'Bloody Index',
+            descripcion: 'Copete, mina, locura',
+            precio: '$4500',
+            imgDrink: 'assets/drinks/bloodyIndex.png'
+        },
+        {
+            id: 9,
+            nombre: 'MojitoStrap',
+            descripcion: 'Amargór en tu paladar',
+            precio: '$5100',
+            imgDrink: 'assets/drinks/mojitostrap.png'
         },
     ])
 return(
