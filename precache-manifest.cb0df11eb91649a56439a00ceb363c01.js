d3ab8f7ee99442d3813ee304ceb738483303478b
BootNM <nimeneses.dwfsc2@bootcampudd.cl>
2022-11-26 22:34:03 -0300

    Updates

---
+++ precache-manifest.cb0df11eb91649a56439a00ceb363c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c5d505f47c184172803",
+    "url": "/bootbar/static/css/main.44f2a994.chunk.css"
+  },
+  {
+    "revision": "2c5d505f47c184172803",
+    "url": "/bootbar/static/js/main.2c5d505f.chunk.js"
+  },
+  {
+    "revision": "948fffa9dd79bf073821",
+    "url": "/bootbar/static/css/1.bded932c.chunk.css"
+  },
+  {
+    "revision": "948fffa9dd79bf073821",
+    "url": "/bootbar/static/js/1.948fffa9.chunk.js"
+  },
+  {
+    "revision": "2eb12f7ccd98ce87ef12",
+    "url": "/bootbar/static/js/runtime~main.2eb12f7c.js"
+  },
+  {
+    "revision": "8788b729fb5631a9027add558590bb7e",
+    "url": "/bootbar/index.html"
+  }
+];
